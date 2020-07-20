147f1a428649f89351a18063f41b346182054a19
sanghak yi <sang105kr@gmail.com>
2020-07-20 16:29:44 +0900

    2020.07.20 pom.xml: db연동추가, junit추가 root-context.xml:db연동 환경설정(jdbctemplate,mybatis) DbConnection.java : db연동 테스트 추가

---
+++ springedu/src/test/java/com/kh/portpolio/DBconnection.java
@@ -0,0 +1,58 @@
+package com.kh.portpolio;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class) //스프링 컨텍스트와 junit통합
+@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+public class DBconnection {
+	private static final Logger logger =
+			LoggerFactory.getLogger(DBconnection.class);
+	
+	@Inject
+	DataSource dataSource;
+	
+	@Inject
+	JdbcTemplate jdbcTemplate;
+	
+	@Test
+	void testdb() {
+		logger.info("DataSource		:" + dataSource);
+		logger.info("JdbcTemplate :" + jdbcTemplate);
+		
+		try {
+			Connection con = dataSource.getConnection();
+			Assertions.assertNotNull(con);
+			logger.info("DB연결 성공!!");
+		} catch (SQLException e) {
+			logger.info("DB연결 실패!");
+			e.printStackTrace();
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
